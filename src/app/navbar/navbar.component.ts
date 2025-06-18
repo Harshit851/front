78d7bcb2c35ddf0d6be2c842054053e5973a7c42
Harshit Verma <Harshit.chaudhary2732@gmail.com>
2025-06-18 15:03:27 +0530

    cart count

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AppState } from '../state/app.state';
-import { logout } from '../state/auth.actions';
-import { RouterModule } from '@angular/router';
 import { selectUser } from '../state/auth.selectors';
+import { selectCartItems } from '../state/cart.selectors';
+import { logout } from '../state/auth.actions';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   user$: Observable<{ email?: string; username?: string; role?: string } | null>;
+  cartCount$: Observable<number>;
 
   constructor(private store: Store<AppState>) {
     this.user$ = this.store.select(selectUser);
+    this.cartCount$ = this.store.select(selectCartItems).pipe(
+      map((items: any[]) =>
+        items.reduce((total: number, item: any) => total + item.quantity, 0)
+      )
+    );
   }
 
   onLogout() {
